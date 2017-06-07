3dcc368ff71c53e58adba17a180ff7a7c02c5d0d
giorgiamarini <giorgiaj.marini@gmail.com>
2017-06-07 09:00:36 +0200

    Aggiunta classe SecurityConfig.

---
+++ siw2017-gallery/src/main/java/it/uniroma3/security/SecurityConfig.java
@@ -0,0 +1,43 @@
+package it.uniroma3.security;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+	@Autowired
+	private DataSource dataSource; 
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+
+		auth.jdbcAuthentication().dataSource(dataSource)
+		.passwordEncoder(new BCryptPasswordEncoder())
+		.usersByUsernameQuery("SELECT username, password,1 FROM users where username=?")
+		.authoritiesByUsernameQuery("SELECT username, authority from authorities where username=?"); 
+	}
+
+	protected void config(HttpSecurity http) throws Exception{
+
+		http
+		.authorizeRequests()
+		.anyRequest().authenticated()
+		.and()
+		.formLogin()
+		.loginPage("/login")
+		.permitAll()
+		.and()
+		.logout()
+		.permitAll();
+	}
+}
